9d75d3967e66698e6f35985ce1fcf1c37a61f9db
Ryan Coones <wrcoones@gmail.com>
2025-11-11 11:04:26 -0500

    fixed saving the arrays

--- data_extraction.py
+++ data_extraction.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 import os
+from pathlib import Path
+
 DIR = "ml-1m/"
 MOVIES = "movies.txt"
 RATINGS = "rate.txt"
 USERS = "users.txt"
 
 def get_all_features_numpy():
-    pickle_path = "movie_arrays/arrays.pkl"
+    pickle_path = Path("movie_arrays/arrays.pkl")
     if os.path.exists(pickle_path):
         print("Loading from pickle...")
 
@@ -18,6 +20,7 @@ def get_all_features_numpy():
         ratings_arr = pd.read_csv(DIR + RATINGS,delimiter="::",engine="python").to_numpy()
         users_arr = pd.read_csv(DIR + USERS,delimiter="::",engine="python").to_numpy()
         all_arrays = (movies_arr,ratings_arr,users_arr)
+        pickle_path.parent.mkdir(parents = True, exist_ok = True)
         pd.to_pickle(all_arrays, pickle_path)
         print(f'len movie ar: {len(movies_arr)}')
     return movies_arr, ratings_arr, users_arr
