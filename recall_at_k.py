24954cb52f41c8471913845b8f1891d3c107fb2a
zains50 <zain.syed0165@gmail.com>
2025-11-11 15:02:44 -0500

    Added dataloder for faster batching

--- recall_at_k.py
+++ recall_at_k.py
@@ -2,5 +2,9 @@ import torch
 
 def recall_at_k_(NUM_USER, NUM_ITEM,K,model):
     batches = 5
-    BATCH_USER = NUM_USER // 5
+    USER_BATCH = NUM_USER // batches
+    NUM_USER = list(range(NUM_USER))
+    for x in range(batches):
+        batch_users = USER_BATCH[:]
+
 
