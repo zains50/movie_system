6b78ff888a24abc3423faa957522193b9aa1c8bc
Ryan Coones <wrcoones@gmail.com>
2025-11-10 20:24:42 -0500

    bpr loss

--- data_extraction.py
+++ data_extraction.py
@@ -7,15 +7,15 @@ RATINGS = "rate.txt"
 USERS = "users.txt"
 
 def get_all_features_numpy():
-    movies_arr = pd.read_csv(DIR + MOVIES, sep="::", engine="python", encoding="latin1")
-    raitngs_arr = pd.read_csv(DIR + RATINGS,delimiter="::").to_numpy()
-    users_arr = pd.read_csv(DIR + USERS,delimiter="::").to_numpy()
-    return movies_arr, raitngs_arr, users_arr
+    movies_arr = pd.read_csv(DIR + MOVIES, sep="::", engine="python", encoding="latin1").to_numpy()
+    ratings_arr = pd.read_csv(DIR + RATINGS,delimiter="::",engine="python").to_numpy()
+    users_arr = pd.read_csv(DIR + USERS,delimiter="::",engine="python").to_numpy()
+    return movies_arr, ratings_arr, users_arr
 
 
 
 def get_movies_numpy():
-    movie_arr = pd.read_csv(DIR + MOVIES,delimiter="::")
+    movie_arr = pd.read_csv(DIR + MOVIES,delimiter="::",engine="python")
     movies_arr = movie_arr.to_numpy()
     return movies_arr
 
