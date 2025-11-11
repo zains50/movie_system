e8452322018a302016c54f119b9643c1957519da
zains50 <zain.syed0165@gmail.com>
2025-11-10 22:56:46 -0500

    added pickling for fast file load

--- data_extraction.py
+++ data_extraction.py
@@ -1,15 +1,25 @@
 import numpy as np
 import pandas as pd
-
+import os
 DIR = "ml-1m/"
 MOVIES = "movies.txt"
 RATINGS = "rate.txt"
 USERS = "users.txt"
 
 def get_all_features_numpy():
-    movies_arr = pd.read_csv(DIR + MOVIES, sep="::", engine="python", encoding="latin1").to_numpy()
-    ratings_arr = pd.read_csv(DIR + RATINGS,delimiter="::",engine="python").to_numpy()
-    users_arr = pd.read_csv(DIR + USERS,delimiter="::",engine="python").to_numpy()
+    pickle_path = "movie_arrays/arrays.pkl"
+    if os.path.exists(pickle_path):
+        print("Loading from pickle...")
+
+        movies_arr,ratings_arr,users_arr = pd.read_pickle(pickle_path)
+    else:
+        print("Reading Raw Data")
+        movies_arr = pd.read_csv(DIR + MOVIES, sep="::", engine="python", encoding="latin1").to_numpy()
+        ratings_arr = pd.read_csv(DIR + RATINGS,delimiter="::",engine="python").to_numpy()
+        users_arr = pd.read_csv(DIR + USERS,delimiter="::",engine="python").to_numpy()
+        all_arrays = (movies_arr,ratings_arr,users_arr)
+        pd.to_pickle(all_arrays, pickle_path)
+
     return movies_arr, ratings_arr, users_arr
 
 
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import torch
-from mf_model import MLP_model
+# from mf_model import MLP_model
+from get_user_movie_features import generate_movie_features, generate_user_features
 
+movies_features, user_features = generate_movie_features(), generate_user_features()
+
+print(f'loaded movie and user features')
+
+epochs = 100
+model_layers = [
+    (2,64),
+    (64,64),
+    (64,64)
+]
